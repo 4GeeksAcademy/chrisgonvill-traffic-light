5654142c3a23cd3848d9d585043093ee4546ee3d
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2023-10-19 00:25:11 +0000

    trafficLight

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "./home.css"; // Asegúrate de importar tu archivo CSS para los estilos
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [color, setColor] = useState("red");
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <div className="traffic-light-container">
+		<div className="traffic-light-box">
+		  <div
+			className={`light red ${color === "red" ? "active" : ""}`}
+			onClick={() => setColor("red")}
+		  ></div>
+		  <div
+			className={`light yellow ${color === "yellow" ? "active" : ""}`}
+			onClick={() => setColor("yellow")}
+		  ></div>
+		  <div
+			className={`light green ${color === "green" ? "active" : ""}`}
+			onClick={() => setColor("green")}
+		  ></div>
 		</div>
+	  </div>
 	);
-};
-
-export default Home;
+  };
+  
+  export default Home;
+//   En este código, los tres focos (div.light) están contenidos dentro de un contenedor rectangular negro (div.traffic-light-box). Puedes ajustar las dimensiones y los estilos según tus necesidades específicas.
+  
+  
+  
+  
+  
+  
